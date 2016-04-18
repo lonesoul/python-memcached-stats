29d8bff7795ef5945cbf77c8e5c49dfa74629208
lonesoul <448765461@qq.com>
2016-04-18 15:03:21 +0800

    Update memcached_stats.py

--- src/memcached_stats.py
+++ src/memcached_stats.py
@@ -19,8 +19,8 @@ class MemcachedStats:
 
     def command(self, cmd):
         ' Write a command to telnet and return the response '
-        self.client.write("%s\n" % cmd)
-        return self.client.read_until('END')
+        self.client().write("%s\n" % cmd)
+        return self.client().read_until('END')
 
     def key_details(self, sort=True, limit=100):
         ' Return a list of tuples containing keys and details '
